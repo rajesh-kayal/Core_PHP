ee7a8b85fa8fd10851572529aa61ccd8b1320a5c
Rajesh Kayal <119810174+rajesh-kayal@users.noreply.github.com>
2023-07-23 02:11:50 +0530

    Add files via upload

---
+++ displayclient.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$id=$_SESSION['id'];
+include('connection.php');
+$sql = "SELECT * FROM multi_user WHERE id='$id'";
+$data = mysqli_query($conn,$sql);
+
+?>
+
+<span>Welcome <?php echo $_SESSION['name'] ?></span>
+<table border="1" cellpadding="10px" cellspacing="0">
+	<tr>
+		<th>SL.NO</th>
+		<th>Name</th>
+		<th>Email</th>
+		<th>Password</th>
+		<th>Phone</th>
+		<th>user</th>
+	</tr>
+	<?php
+	$i =1;
+	while($result=mysqli_fetch_assoc($data))
+	{
+		?>
+		<tr>
+			<td><?php echo $i;$i++;?></td>
+			<td><?php echo $result['name']?></td>
+			<td><?php echo $result['email']?></td>
+			<td><?php echo $result['password']?></td>
+			<td><?php echo $result['phone']?></td>
+			<td><?php echo $result['user']?></td>
+		<?php
+		//$i++;
+	}
+	?>
+</table>
---
+++ loginaction.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include('connection.php');
+$email = $_REQUEST['email'];
+$pass = md5($_REQUEST['pass']);
+
+$sql = "SELECT * FROM multi_user WHERE email='$email' AND password='$pass'";
+$data = mysqli_query($conn, $sql);
+$total = mysqli_num_rows($data);
+$result = mysqli_fetch_assoc($data);
+$_SESSION['name']=$result['name'];
+if ($total) {
+            
+                if($result['user']=='admin'){
+                header('Location:displayclients.php');
+            }else{
+                $_SESSION['id']=$result['id'];
+                header('Location:displayclient.php');
+            }
+            
+
+
+} else {
+    echo "Login failed!.";
+}
+
+
+
+
+
+
+
+
+
+
+// $sql = "SELECT * FROM multi_user WHERE email='$email' AND password='$pass' AND user='client'";
+// $data = mysqli_query($conn, $sql);
+// $result = mysqli_fetch_assoc($data);//"mysqli_fetch_assoc retrieves a single row of data as an associative array from a database query result."
+// if ($result) {
+// 	 $_SESSION['id'] = $result['id'];
+//     header("Location:displayclient.php");
+// } else {
+//     $sql = "SELECT * FROM multi_user WHERE email='$email' AND password='$pass' AND user='admin'";
+//     $data = mysqli_query($conn, $sql);
+//     $result = mysqli_num_rows($data);//mysqli_num_rows is used to check if user data exists in our table. If the user exists, it returns all of their data.
+
+//     if ($result) {
+//         header("Location:displayclients.php");
+//     } else {
+//         echo "Login failed";
+//     }
+// }
+// ?>
